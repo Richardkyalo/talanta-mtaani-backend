cae65bc3ecd9a5e5cc15331165dc34069e749e6f
RichardKyalo <richardkyalo57@gmail.com>
2024-11-06 15:35:35 +0300

    setting up user

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || 'mysql',
+  },
+};
